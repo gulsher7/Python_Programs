f20e613746708c59d93ed50406e4f5afacbc5dca
gulsher <gulsherkhan0005@gmail.com>
2019-11-02 17:06:18 +0530

    add function programs

---
+++ functions/workingfunc.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter number"))
+num2 = int(input("enter number"))
+num3 = int(input("enter number"))
+
+
+def add(x,y,z):
+    return x + y + z
+
+result = add(num1, num2, num3)
+print(result)
+
---
+++ infiniteloop.py
@@ -0,0 +1,6 @@
+num = input("enter a number ")
+sum = 0
+
+for x in num:
+    sum += int(x)
+print(sum)
