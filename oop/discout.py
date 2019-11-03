092421569cb7bad8b3dac09156af61accbdb458d
gulsher <gulsherkhan0005@gmail.com>
2019-11-03 09:09:15 +0530

    discount program

--- oop/discout.py
+++ oop/discout.py
@@ -5,17 +5,19 @@ class Laptop:
         self.laptop_price = price
 
     def discount(self,dis):
-        self.dis = (self.laptop_price * dis)/100
+        self.discount_price = (dis/100)*self.laptop_price
+        self.discount_price = self.laptop_price - self.discount_price 
+
         
     def full_detail(self):
         return f"{self.laptop_name} {self.laptop_model_no} {self.laptop_price} "
 
 
 
-obj1 = Laptop('hp', 'i5', 58970)
+obj1 = Laptop('hp', 'i5', 50000)
 print(obj1.full_detail())
 
 print("original price is :", obj1.laptop_price)
-obj1.discount(12)
-print("after 12% discount")
-print(obj1.dis)
+obj1.discount(10)
+print("after 10% discount")
+print(obj1.discount_price)
